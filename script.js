fc87dd38ce8a1473e8aa444e8736cb7d4166bfe0
Muscalu Darius <muscaludarius4@gmail.com>
2021-12-20 19:53:05 +0200

    Almost finished the mobile design. only a few tweaks left

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ snowStorm.flakesMaxActive = 96;
 snowStorm.useTwinkleEffect = true; 
 // 4. Start the snowstorm!
 snowStorm.start();
+
+// These 2 dosen't work as it supposed, resolve them
+function darkMode() {
+    let element = document.body;
+    element.classList.toggle("darkMode");
+}
+
+function lightMode() {
+    let element = document.body;
+    element.classList.toggle("lightMode");
+}
+
+// Displays the information about the project when the 'about project' is clicked
+function displayProjectInfo() {
+    document.getElementById("project-info").style.display = "block";
+}
